03282cca3576b368daaf5d2c513744c8b4fce2d2
Mahak-Agrawal16 <agrawalmahak161204@gmail.com>
2026-01-15 14:25:42 +0530

    added stack using list python code

--- stackusinglist.py
+++ stackusinglist.py
@@ -1,1 +1,7 @@
-stack=
+stack=[]
+n=int(input("Enter number of elements: "))
+for i in range(n):
+    stack.append(input("enter element: "))
+print("Stack:",stack)
+stack.pop()
+print("After pop:",stack)
